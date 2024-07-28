744166f68dfcb7670b11fe818e865ad9e6e9735d
0xMailMan <chengye.pcy@gmail.com>
2024-07-28 17:40:52 +0800

    added auto cycling homepage ads

--- App/src/components/Home.jsx
+++ App/src/components/Home.jsx
@@ -1,38 +1,112 @@
-import React from 'react';
-import home_background from '../assets/home_advert.png';
-import {getUserData} from '../../utils/userdata';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import ad1 from '../assets/HOMEPAGE_ADS_ASSETS/HOMEPAGE_AD_1.png';
+import ad2 from '../assets/HOMEPAGE_ADS_ASSETS/HOMEPAGE_AD_2.png';
+import ad3 from '../assets/HOMEPAGE_ADS_ASSETS/HOMEPAGE_AD_3.png';
+import ad4 from '../assets/HOMEPAGE_ADS_ASSETS/HOMEPAGE_AD_4.png';
+import { getUserData } from '../../utils/userdata';
+import './home.css';
+
+const advertsData = [
+  {
+    name: "HOMEPAGE_AD_1",
+    image: ad1,
+    header: "Craving a Holiday?",
+    body_text: "Forget about the fuss of getting there: we are sure you'll like our one-click vacation packages!\n\nFrom the tropical getaways of Bali to the snowy alps of Switzerland, you're sure to find an adventure that interests you.\n\nLimited slots available! What are you waiting for?",
+    cta_button: "Bring Me There!"
+  },
+  {
+    name: "HOMEPAGE_AD_2",
+    image: ad2,
+    header: "No More Worries.",
+    body_text: "Is your current car insurance giving you a headache? Say no to complicated plans and check out our Supreme Coverage Plan!\n\nInsurance that is transparent, cost-effective, and easy to understand: Say bye-bye to all the stress!",
+    cta_button: "Register Now"
+  },
+  {
+    name: "HOMEPAGE_AD_3",
+    image: ad3,
+    header: "Celebrate Christmas with Royal Air!",
+    body_text: "Fly like a royal this Christmas! We are delighted to partner with Royal Air to giveaway discount vouchers worth up to $500 to 10 lucky winners!\n\nSimply follow a few easy steps and stand a chance to win; terms and conditions apply. Sign ups close on Nov 15.",
+    cta_button: "Sign Up Now"
+  },
+  {
+    name: "HOMEPAGE_AD_4",
+    image: ad4,
+    header: "Gift for That Special Someone This Season.",
+    body_text: "Surprise a special someone this season with our limited edition credit card, jam packed with seasonal promotions and perks!\n\nBy giving someone this card, you can stand a chance to win an all-inclusive travel package! What are you waiting for? Spread the joy!",
+    cta_button: "Find Out More!"
+  }
+];
+
 function Home() {
   const [userData, setUserData] = useState({
     firstName: '',
     lastName: '',
     points: 0,
-  })
+    user_id: ''
+  });
+  const [currentAdvert, setCurrentAdvert] = useState(0);
+
   useEffect(() => {
-    const data = getUserData();
-    setUserData(data);
+    try {
+      const data = getUserData();
+      setUserData(data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+
+    const interval = setInterval(() => {
+      setCurrentAdvert((prevAdvert) => (prevAdvert + 1) % advertsData.length);
+    }, 5000); // Change advert every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
   }, []);
+
+  const handlePrevClick = () => {
+    setCurrentAdvert((prevAdvert) => (prevAdvert - 1 + advertsData.length) % advertsData.length);
+  };
+
+  const handleNextClick = () => {
+    setCurrentAdvert((prevAdvert) => (prevAdvert + 1) % advertsData.length);
+  };
+
+  if (advertsData.length === 0) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div style={{ position: 'relative', width: '100%' }}>
-      <img src={home_background} alt="Home" style={{ width: '100%' }} />
-      <div
-        className="text"
-        style={{
-          position: 'absolute',
-          top: '10px',
-          left: '10px',
-          color: 'white',
-          padding: '10px',
-      
-        }}
-      >
-        <h1>Home</h1>
-        <p>Welcome to the Home page, {userData.firstName} {userData.lastName}.</p>
+    <div>
+      <div className="home-container">
+        <div className="image-container">
+          <img src={advertsData[currentAdvert].image} alt="Home" className="home-image" />
+          <div className="black-gradient-overlay"></div>
+          <div className="nav-buttons">
+            <button className="nav-button" onClick={handlePrevClick}>
+              Prev
+            </button>
+            <button className="nav-button" onClick={handleNextClick}>
+              Next
+            </button>
+          </div>
+        </div>
+        <div className="text-container">
+          <h1>{advertsData[currentAdvert].header}</h1>
+          <p>{advertsData[currentAdvert].body_text.split('\n').map((text, index) => (
+            <span key={index}>{text}<br /></span>
+          ))}</p>
+          <button className="cta-button">
+            {advertsData[currentAdvert].cta_button}
+          </button>
+        </div>
+      </div>
+      <div className="user-info-container">
+        <h1>Welcome Back to FETCH!</h1>
+        <p>Hello, {userData.firstName} {userData.lastName}.</p>
         <p>You have, {userData.points} points.</p>
-        <p>Membership Id is {userData.user_id}. </p>
+        <p>Membership Id is {userData.user_id}.</p>
       </div>
     </div>
-  );
+  );  
+
 }
 
 export default Home;
--- App/src/components/Login.jsx
+++ App/src/components/Login.jsx
@@ -48,36 +48,33 @@ const Login = ({ onLogin }) => {
 
   return (
     <div className="login-page">
-    <img className="logo" src={bankLogo} alt="Fetch Banking Logo" />
-
-      <div className="container">
-      <img className="dog-login" src={dogLogin} alt="Dog Login" />
-        <div className="login-box">
-          <h2>Welcome Back!</h2>
-          <form onSubmit={handleSubmit}>
-            <label>Login</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Username"
-              required
-            />
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-            />
-            <button type="submit" className="login-button">Let's Go</button>
-            <p>{message}</p>
-            <p>New Here? <Link to="/register">Register</Link></p>
-            
-          </form>
-        
+        <img className="logo" src={bankLogo} alt="Fetch Banking Logo" />
+        <img className="dog-login" src={dogLogin} alt="Dog Login" />
+        <div className="container">
+            <div className="login-box">
+                <h2>Welcome Back!</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>Login</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Username"
+                        required
+                    />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                        required
+                    />
+                    <button type="submit" className="login-button">Let's Go</button>
+                    <p>{message}</p>
+                    <p>New Here? <Link to="/register">Register</Link></p>
+                </form>
+            </div>
         </div>
-      </div>
     </div>
   );
 };
