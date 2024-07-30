133211da26b4ca41efa314f077e70ba0459d0829
0xMailMan <chengye.pcy@gmail.com>
2024-07-31 02:20:29 +0800

    improved component UI

--- App/src/components/profileHelper/TierProgress.jsx
+++ App/src/components/profileHelper/TierProgress.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import rewardIcon from '../../assets/GRAPHIC_ASSETS/GRAPHIC_GOLDEN_BONE_TIER.svg'
 
 const TierProgress = () => {
-  return (
-    <div className="tier-info">
-      <h3>Tier I : Bronze</h3>
-      <div className="tier-progress">
-        <div className="progress-bar">
-          <div className="progress" style={{ width: '20%' }}></div>
+    return (
+      <div className="tier-info">
+        <h3>Tier I : Bronze</h3>
+        <div className="tier-progress-container">
+          <div className="tier-progress">
+            <div className="progress-bar">
+              <div className="progress" style={{ width: '20%' }}></div>
+            </div>
+            <img src={rewardIcon} alt="Reward Icon" className="reward-icon" />
+          </div>
+          <p>Treats Left to Tier II : Silver <span>80</span></p>
+          <div className="reward-details">
+            <p>Reward for completing this tier: <span>1x Golden Bone</span></p>
+            <button className="claim-button">CLAIM</button>
+          </div>
         </div>
-        <p>Treats Left to Tier II : Silver <span>80</span></p>
-        <p>Reward for completing this tier: <span>1x Golden Bone</span></p>
-        <button className="claim-button">CLAIM</button>
       </div>
-    </div>
-  );
+    );
 };
 
 export default TierProgress;
--- App/src/components/profileHelper/UserDetails.jsx
+++ App/src/components/profileHelper/UserDetails.jsx
@@ -7,15 +7,21 @@ const UserDetails = () => {
 
   return (
     <div className="profile-card">
-      <div className="profile-picture">
-        <img src={defaultProfilePicture} alt="Profile" />
+      <div className="profile-top-section">
+        <div className="profile-picture">
+          <img src={defaultProfilePicture} alt="Profile" />
+        </div>
+        <div className="profile-details">
+          <h2>{user.firstName} {user.lastName}</h2>
+          <p>Email: john.doe@example.com</p>
+        </div>
+        <button className="edit-profile-button">Edit</button>
       </div>
-      <div className="profile-details">
-        <h2>{user.firstName} {user.lastName}</h2>
-        <p>Email: john.doe@example.com</p>
+      <div className="profile-bottom-section">
         <p className="bank-points">Bank Points Balance: <span>{user.points}</span></p>
+      </div>
+      <div className="golden-bones-section">
         <p className="golden-bones">Golden Bones: <span>0</span></p>
-        <button className="edit-profile-button">Edit Profile</button>
       </div>
     </div>
   );
